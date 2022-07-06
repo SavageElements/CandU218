d34f929b9e7d6b88a45539775395f5b1475ddc54
J Rockwell Seebach <jrs@ironpinephoto.com>
2022-07-06 16:07:05 -0700

    Add project files.

---
+++ CandU218/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CandU218
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            int dataInput = Convert.ToInt16(Console.ReadLine());
+
+            
+            int result3 = Class1.AddSeven(dataInput);
+            Console.WriteLine("Your number + 7 is " + result3);
+
+            Console.WriteLine("Please enter a decimal.");
+
+            decimal dataInput2 = Convert.ToDecimal(Console.ReadLine());
+            int nextUP = Class1.AddSeven(dataInput2);
+            Console.WriteLine("Your number + 8 is " + nextUP);
+
+            Console.WriteLine("Please enter another number.");
+
+            string dataInput3 = Convert.ToString(Console.ReadLine());
+            int nextUP2 = Class1.AddSeven(dataInput3);
+            Console.WriteLine("Your number + 9 is " + nextUP2);
+
+
+        }
+    }
+}
---
+++ CandU218/classOne.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CandU218
+{
+    class Class1
+    {
+        public static int AddSeven(int X)
+        {
+            int M3 = X;
+            int plusSEV = M3 + 7;
+            return plusSEV;
+        }
+        public static int AddSeven(decimal y)
+        {
+            decimal M4 = y;
+            decimal plusOcho = M4 + 8;
+            return Convert.ToInt16(plusOcho);
+        }
+
+        public static int AddSeven(string z)
+        {
+            int M5 = Convert.ToInt16(z);
+            int plusNine = M5 + 9;
+            return plusNine;
+        }
+    }
+}
